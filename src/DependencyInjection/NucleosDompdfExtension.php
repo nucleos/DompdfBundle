1c93dff662ffd60dcb9f58e82674aade87f7bdc0
Kitee666 <18664027+Kitee666@users.noreply.github.com>
2024-08-08 14:46:13 +0200

    Update NucleosDompdfExtension.php Extension Injection

--- src/DependencyInjection/NucleosDompdfExtension.php
+++ src/DependencyInjection/NucleosDompdfExtension.php
@@ -14,7 +14,7 @@ namespace Nucleos\DompdfBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 final class NucleosDompdfExtension extends Extension
 {
